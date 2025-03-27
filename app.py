23f343d523e18e9161d6cd8c168c6166202e86df
saikiranreven <rsksislvgy@gmail.com>
2025-03-27 18:09:16 -0230

    real

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
+from datetime import datetime
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello from Cloud Run!"
+def show_time():
+    return render_template('clock.html',
+        current_time=datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        timezone=os.getenv('TZ', 'UTC')
+    )
 
 if __name__ == '__main__':
-    # Use GUNICORN_PORT with fallback
-    app.run(host='0.0.0.0', port=int(os.environ.get('GUNICORN_PORT', 8080)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
